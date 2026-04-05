7ab9ac8022f695d15e39d2af18f279ffbac81b42
joelyakubu269 <joelyakubu269@gmail.com>
2026-04-05 17:03:18 +0100

    trying to print commits

--- main.go
+++ main.go
@@ -7,23 +7,24 @@ import (
 
 func main() {
 	var username string
+	var maxEvents int
 	flag.StringVar(&username, "user", "", "github  username to lookup info on.")
 	flag.BoolVar(&logEnabled, "useloud", false, "set to enable verbose logging")
+	flag.IntVar(&maxEvents, "maxevents", 5, "limit maximum nummber of events printed")
 	flag.Parse()
 	if flag.NArg() > 0 {
 		fmt.Println("no arguements expected")
 		return
 	}
-	event, err := fetchApi(username)
+	events, err := fetchApi(username)
 	if err != nil {
 		fmt.Println("Error fetching events:", err, "for user", username)
 		return
 	}
-	if len(event) == 0 {
-		fmt.Println("There is no event to be fetched")
-		return
+	if len(events) > maxEvents {
+		events = events[len(events)-5:]
 	}
-	printEvent(event)
+	printEvent(events)
 }
 
 var logEnabled bool
