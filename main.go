6cd10da669d0fa6bf844f21cc9f4b1a19cd840d7
joelyakubu269 <joelyakubu269@gmail.com>
2026-04-05 16:39:03 +0100

    trying to print commits

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("go run . <github-username>")
+	var username string
+	flag.StringVar(&username, "user", "", "github  username to lookup info on.")
+	flag.BoolVar(&logEnabled, "loud", false, "set to enable verbose logging")
+	flag.Parse()
+	if flag.NArg() > 0 {
+		fmt.Println("no arguements expected")
 		return
 	}
-	username := os.Args[1]
 	event, err := fetchApi(username)
 	if err != nil {
 		fmt.Println("Error fetching events:", err)
@@ -21,4 +24,12 @@ func main() {
 		return
 	}
 	printEvent(event)
+}
+
+var logEnabled bool
+
+func logstuff(args ...any) {
+	if logEnabled {
+		fmt.Println(args...)
+	}
 }
